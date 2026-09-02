9b529153bc5324638052465b25f22421550e091a
pranav gupta <pranavgupta32006@gmail.com>
2026-09-02 14:49:22 +0530

    feat: add hello world examples in multiple programming languages and update language registry

---
+++ POLYGLOT FOUNDATION/first.cs
@@ -0,0 +1,8 @@
+namespace MyProgram {
+    class Program {
+        static void Main(string[] args) {
+            int x = 42;
+            System.Console.WriteLine(x);
+        }
+    }
+}
---
+++ POLYGLOT FOUNDATION/first.ts
@@ -0,0 +1,3 @@
+let x: number = 42;
+console.log(x);
+export{};  // js and ts file in same directory so the x varible waaas repeating , so we this file as module ,, by writing export{}
